f69372093d86931b9f918a685bf450bd337cd371
TravelTec <59958315+TravelTec@users.noreply.github.com>
2023-06-12 15:54:57 -0300

    Altera versão

--- vouchertec-ehtl.php
+++ vouchertec-ehtl.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 	Description: Voucher Tec - Integração de hotéis E-htl é um plugin desenvolvido para agências e operadoras de turismo que precisam tratar diárias de hospedagem de fornecedores, com integração ao fornecedor E-htl.
 
-	Version: 1.1.4
+	Version: 1.1.5
 
 	Author: Travel Tec
 
